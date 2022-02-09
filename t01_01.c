365713ecc161ec305bacb8e18aceb3f64dc0ae80
Sitogab Antonio <antonio.girsang@gmail.com>
2022-02-09 14:08:51 +0700

    melakukan percobaan pertama

--- t01_01.c
+++ t01_01.c
@@ -1,11 +1,21 @@
-// NIM - Name
-// NIM - Name
+// 12S21015 - Sitogab Antonio Octavianus Girsang
+// 12S21049 - Jesika Audina Purba
 
 #include <stdio.h>
 
-int main(int _argv, char **_argc)
+int main (int _argv, char **_argc)
 {
-  // codes
+    int num;
+      char ch;
+      int num2;
+      float num3
 
+      scanf ("%d", &num);
+      scanf (" %c", &ch);
+      scanf ("%d", &num2);
+      scanf ("%f", &num3);
+
+      printf ("%d\n%c\n%d\n%.3f\n", num, ch, num2, num3);
+  
   return 0;
 }
--- t01_02.c
+++ t01_02.c
@@ -1,11 +1,30 @@
-// NIM - Name
-// NIM - Name
+// 12S21015 - Sitogab Antonio Octavianus Girsang
+// 12S21049 - Jesika Audina Purba
 
 #include <stdio.h>
 
-int main(int _argv, char **_argc)
-{
-  // codes
+int main(){
+	
+	float p, q, r, s, hasil, rata;
+
+
+printf("masukan angka: ");
+scanf("%f",&r);
+printf("masukan angka ");
+scanf("%f",&p);
+printf("masukan angka ");
+scanf("%f",&q);
+printf("masukan angka ");
+scanf("%f",&s);
+
+	hasil = p + q + r + s;
+	printf("hasil nya adalah = %f\n", hasil);
+
+	rata= hasil / 4;
+	printf("rata-rata adalah = %f", rata);
+
+
+
 
   return 0;
 }
